5243552291ddf7d54c6076ce3701a658eae29873
Johnny Bananas <johnny.whichard@fuiszmedia.com>
2016-02-26 14:22:45 -0800

    Removed Old Contact Form...Updated Connect Section

--- Bootstrap-Agency/mail/contact_me.php
+++ Bootstrap-Agency/mail/contact_me.php
@@ -19,7 +19,7 @@ $message = $_POST['message'];
 $to = 'johnnyvwhichard@alumni.nd.edu'; // Add your email address inbetween the '' replacing yourname@yourdomain.com - This is where the form will send a message to.
 $email_subject = "Website Contact Form:  $name";
 $email_body = "You have received a new message from your website contact form.\n\n"."Here are the details:\n\nName: $name\n\nEmail: $email_address\n\nPhone: $phone\n\nMessage:\n$message";
-$headers = "From: noreply@johnnyBananas1.github.io\n"; // This is the email address the generated message will be from. We recommend using something like noreply@yourdomain.com.
+$headers = "From: noreply@johnnyBananas1.com.github.io\n"; // This is the email address the generated message will be from. We recommend using something like noreply@yourdomain.com.
 $headers .= "Reply-To: $email_address";	
 mail($to,$email_subject,$email_body,$headers);
 return true;			
