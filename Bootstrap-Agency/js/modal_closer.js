5b163577b52c72c2fb137761cdfdadf90f480d6a
Johnny Bananas <johnny.whichard@fuiszmedia.com>
2016-02-29 14:01:04 -0800

    Gallery Overhaul

--- Bootstrap-Agency/js/modal_closer.js
+++ Bootstrap-Agency/js/modal_closer.js
@@ -1,9 +1,21 @@
-$('.close-modal').click(function () {
- $('#portfolioModal1').hide();
- $('#portfolioModal1 iframe').attr("src", jQuery("#portfolioModal1 iframe").attr("src"));
+
+//Stop Video
+
+$('#portfolioModal1').on('hidden.bs.modal', function (e) {
+  $('#portfolioModal1').hide();
+  $('#portfolioModal1 iframe').attr("src", jQuery("#portfolioModal1  iframe").attr("src"));
 });
 
-$('.close-modal').click(function () {
- $('#portfolioModal2').hide();
- $('#portfolioModal2 iframe').attr("src", jQuery("#portfolioModal2 iframe").attr("src"));
+
+
+$('#portfolioModal2').on('hidden.bs.modal', function (e) {
+  $('#portfolioModal1').hide();
+  $('#portfolioModal2 iframe').attr("src", jQuery("#portfolioModal2  iframe").attr("src"));
+});
+
+
+
+$('#portfolioModal3').on('hidden.bs.modal', function (e) {
+  $('#portfolioModal1').hide();
+  $('#portfolioModal3 iframe').attr("src", jQuery("#portfolioModal3  iframe").attr("src"));
 });
