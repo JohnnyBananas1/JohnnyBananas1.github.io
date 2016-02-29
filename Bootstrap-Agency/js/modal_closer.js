be4f5b352605c1774d40efa0762602519b6df1a1
Johnny Bananas <johnny.whichard@fuiszmedia.com>
2016-02-29 15:36:57 -0800

    Gallery Works on Desktop

--- Bootstrap-Agency/js/modal_closer.js
+++ Bootstrap-Agency/js/modal_closer.js
@@ -9,13 +9,33 @@ $('#portfolioModal1').on('hidden.bs.modal', function (e) {
 
 
 $('#portfolioModal2').on('hidden.bs.modal', function (e) {
-  $('#portfolioModal1').hide();
+  $('#portfolioModal2').hide();
   $('#portfolioModal2 iframe').attr("src", jQuery("#portfolioModal2  iframe").attr("src"));
 });
 
 
 
 $('#portfolioModal3').on('hidden.bs.modal', function (e) {
-  $('#portfolioModal1').hide();
+  $('#portfolioModal3').hide();
   $('#portfolioModal3 iframe').attr("src", jQuery("#portfolioModal3  iframe").attr("src"));
+});
+
+$('#portfolioModal4').on('hidden.bs.modal', function (e) {
+  $('#portfolioModal4').hide();
+  $('#portfolioModal4 iframe').attr("src", jQuery("#portfolioModal4  iframe").attr("src"));
+});
+
+$('#portfolioModal5').on('hidden.bs.modal', function (e) {
+  $('#portfolioModal5').hide();
+  $('#portfolioModal5 iframe').attr("src", jQuery("#portfolioModal5  iframe").attr("src"));
+});
+
+$('#portfolioModal6').on('hidden.bs.modal', function (e) {
+  $('#portfolioModal6').hide();
+  $('#portfolioModal6 iframe').attr("src", jQuery("#portfolioModal6  iframe").attr("src"));
+});
+
+$('#portfolioModal7').on('hidden.bs.modal', function (e) {
+  $('#portfolioModal7').hide();
+  $('#portfolioModal7 iframe').attr("src", jQuery("#portfolioModal7  iframe").attr("src"));
 });
